4868beb1fc8be18849fd8e8b231eec40551ae707
Kulamrit <35420334+Kulamrit@users.noreply.github.com>
2020-04-04 09:00:26 +0530

    new2

--- covidtrackerapp/src/app/app.component.ts
+++ covidtrackerapp/src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'covidtrackerapp';
+  
+  orders: any[] = [
+    { id: '1', name: 'Afghanistan' },
+    { id: '2', name: 'Alaska' },
+    { id: '3', name: 'Austria' },
+    { id: '4', name: 'Australia' }
+  ];
 }
+
