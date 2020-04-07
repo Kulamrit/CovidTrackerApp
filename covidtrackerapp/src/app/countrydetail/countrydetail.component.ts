b081d646afedcf8e0a4b07c67644be79215ff9be
Kulamrit <35420334+Kulamrit@users.noreply.github.com>
2020-04-07 10:01:16 +0530

    monday 3

--- covidtrackerapp/src/app/countrydetail/countrydetail.component.ts
+++ covidtrackerapp/src/app/countrydetail/countrydetail.component.ts
@@ -25,7 +25,15 @@ export class CountrydetailComponent implements OnInit {
   }
   img;
   img2;
+  Total_Cases;
+  Total_Deaths;
+  Today_Cases;
+  Today_Deaths;
+  Critical_Cases;
   route_var = this.route.snapshot.params['country'];
+  gotoHome(){
+    this.router.navigate(['']);
+  }
 
   fetchPost() {debugger
     this.http
@@ -33,8 +41,14 @@ export class CountrydetailComponent implements OnInit {
 
       .subscribe(responseData => {debugger
         this.country = Object.values(responseData)[0];
-        this.Active_cases = Object.values(responseData)[7];
-        this.Recovered_Cases = Object.values(responseData)[6];
+        this.Total_Cases = Object.values(responseData)[3];
+        this.Today_Cases = Object.values(responseData)[4];
+        this.Total_Deaths = Object.values(responseData)[5];
+        this.Today_Deaths = Object.values(responseData)[6]; 
+        this.Recovered_Cases = Object.values(responseData)[7];
+        this.Active_cases = Object.values(responseData)[8];
+        this.Critical_Cases = Object.values(responseData)[9];
+       
         this.img = Object.values(responseData)[1];
         this.img2 = Object.values(this.img)[5];
         
